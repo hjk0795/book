4f9d7a6c99a85e12f10598a0e37b9c1bd205596e
hjk0795 <hjk0795@gmail.com>
2022-04-01 19:20:51 +0900

    heroku setting

--- config/db.js
+++ config/db.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "../../book_client/client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../../book_client/client/build", "index.html"));
+});
+
 const connectDB = async () => {
   try {
     await mongoose.connect(
